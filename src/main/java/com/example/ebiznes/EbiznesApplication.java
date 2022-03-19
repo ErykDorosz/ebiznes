dfd930127e26cf904492a38d5ab9ba67866e524b
Eryk Dorosz <228726@edu.p.lodz.pl>
2022-03-19 10:08:54 +0100

    Initial commit

---
+++ src/main/java/com/example/ebiznes/EbiznesApplication.java
@@ -0,0 +1,15 @@
+package com.example.ebiznes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbiznesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EbiznesApplication.class, args);
+    }
+
+}
