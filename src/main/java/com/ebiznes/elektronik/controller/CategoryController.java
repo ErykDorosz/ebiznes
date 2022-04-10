6fb2eccb4328c0642887d0548184b55d3924cf81
maciekbartczak <maciekbartczak17@gmail.com>
2022-04-10 11:14:12 +0200

    image upload handling

--- src/main/java/com/ebiznes/elektronik/controller/CategoryController.java
+++ src/main/java/com/ebiznes/elektronik/controller/CategoryController.java
@@ -4,7 +4,6 @@ import com.ebiznes.elektronik.entity.Category;
 import com.ebiznes.elektronik.repository.CategoryRepository;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/ebiznes/elektronik/service/ProductService.java
+++ src/main/java/com/ebiznes/elektronik/service/ProductService.java
@@ -6,25 +6,58 @@ import com.ebiznes.elektronik.repository.CategoryRepository;
 import com.ebiznes.elektronik.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
+    @Value("${app.uploadDir}")
+    private String uploadDir;
+
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
 
     public Product addProduct(AddProductRequest addProductRequest) {
-        val catgory = categoryRepository.findById(addProductRequest.getCategoryId())
+        val category = categoryRepository.findById(addProductRequest.getCategoryId())
                         .orElseThrow(IllegalArgumentException::new);
+        val image = saveImage(addProductRequest.getImage());
 
         val product = Product.builder()
                 .name(addProductRequest.getName())
                 .unitPrice(addProductRequest.getUnitPrice())
-                .imageFilename("foo")
-                .category(catgory)
+                .imageFilename(image)
+                .category(category)
                 .build();
         return productRepository.save(product);
+    }
+
+    private String saveImage(String imageValue) {
+        try {
+            val base64Image = imageValue.split(",")[1];
+            val imageBytes = Base64.getDecoder().decode(base64Image);
+            val root = Path.of(uploadDir);
+            createDirIfNotExists(root);
+
+            val filename = UUID.randomUUID() + ".jpg";
+            new FileOutputStream(root.resolve(filename).toString()).write(imageBytes);
+            return filename;
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to save image");
+        }
+    }
+
+    private void createDirIfNotExists(Path path) {
+        if (!path.toFile().exists()) {
+            path.toFile().mkdirs();
+        }
     }
 }
